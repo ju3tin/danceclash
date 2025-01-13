ab000f6928ccd6294edee7e4ced86f5519815813
ju3tin <ju3tin@hotmail.co.uk>
2025-01-13 10:41:16 +0000

    update

--- app/(home)/game/page.tsx
+++ app/(home)/game/page.tsx
@@ -1,10 +1,32 @@
+"use client"
 import Image from "next/image";
 import Footer from "../../components/footer";
+import axios from "axios";
+import { useEffect, useState } from 'react';
+import axiosInstance from '../../../lib/axiosInstance';
 
 export default function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axiosInstance.get('/products/1') // Replace with your API endpoint
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   return (
+ 
+  
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       
+       
+      <h1 id="data-from-api">Data from API</h1>
+    {data ? (
+      <pre>{JSON.stringify(data, null, 2)}</pre>) : (<p>Loading...</p> )}
         <Image
           className="dark:invert"
           src="/next.svg"
---
+++ utils/axiosInstance.js
@@ -0,0 +1,45 @@
+// axiosInstance.js
+
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 10000,
+});
+
+
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    const language = localStorage.getItem('language') || 'en';
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    config.headers['Accept-Language'] = language;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem('refreshToken');
+      try {
+        const { data } = await axiosInstance.post('/auth/refresh-token', { token: refreshToken });
+        localStorage.setItem('accessToken', data.accessToken);
+        axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${data.accessToken}`;
+        return axiosInstance(originalRequest);
+      } catch (refreshError) {
+        // Handle token refresh error (e.g., redirect to login)
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+export default axiosInstance;
