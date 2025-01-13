02b0fae308752ad1372c989c1f8b042a75826364
ju3tin <ju3tin@hotmail.co.uk>
2025-01-13 12:12:27 +0000

    sd

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,18 +19,18 @@ export default function footer() {
 </a>
 <a
   className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-  href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+  href="https://github.com/ju3tin"
   target="_blank"
   rel="noopener noreferrer"
 >
   <Image
     aria-hidden
-    src="/window.svg"
+    src="/images/github-mark/github-mark.svg"
     alt="Window icon"
     width={16}
     height={16}
   />
-  Examples
+  @Ju3tin
 </a>
 <a
   className="flex items-center gap-2 hover:underline hover:underline-offset-4"
