8846dd31c9178efa3429a8473bd80787554dcbe6
ju3tin <ju3tin@hotmail.co.uk>
2025-01-13 13:21:49 +0000

    update

--- app/(home)/player/page.tsx
+++ app/(home)/player/page.tsx
@@ -31,7 +31,17 @@ export default function Home() {
   
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <Video src="/videos/1.mp4" />
+     {/* <Video className="video3" src="/videos/1.mp4" /> */}
+      <video width="320" height="240" controls preload="none">
+      <source src="/videos/1.mp4" type="video/mp4" />
+      <track
+        src="/path/to/captions.vtt"
+        kind="subtitles"
+        srcLang="en"
+        label="English"
+      />
+      Your browser does not support the video tag.
+    </video>
        
     {data ? (
       <div className="gallery grid grid-cols-3 gap-4">
