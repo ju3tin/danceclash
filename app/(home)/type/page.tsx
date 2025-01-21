180097f8e39e81b3036ef606340f0f25dd5e5162
ju3tin <ju3tin@hotmail.co.uk>
2025-01-21 20:18:36 +0000

    www

--- app/(home)/type/page.tsx
+++ app/(home)/type/page.tsx
@@ -3,10 +3,22 @@ import React from 'react';
 import DanceClash from '../../components/Dance1';
 
 const DanceClashPage: React.FC = () => {
+  const handlePoseUpdate = (poses: any) => {
+    console.log('Poses updated:', poses);
+  };
+
+  const handleStatusChange = (status: string) => {
+    console.log('Status changed:', status);
+  };
+
   return (
     <div>
       <h1>Dance Clash Game</h1>
-      <DanceClash />
+      <DanceClash
+        videoSource="/path/to/your/video.mp4"
+        onPoseUpdate={handlePoseUpdate}
+        onStatusChange={handleStatusChange}
+      />
     </div>
   );
 };
